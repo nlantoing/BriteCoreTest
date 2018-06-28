07fc34a638f016ec5d4023e3658c46e938f3c2d8
Nicolas LANTOING <haulric@gmail.com>
2018-06-28 18:48:48 +0200

    Fix db_mock script

--- db_mock.py
+++ db_mock.py
@@ -1,38 +1,30 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from database import Client, Request, Product_Area, Base
+from flask import jsonify, request, render_template
+from database import Client, Request, Product_Area, db, app
 
 import datetime
-
-engine = create_engine('sqlite:///britecore.db')
-Base.metadata.bind = engine
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-session.add_all([
+db<<<.create_all()
+db.session.add_all([
     Client(name='Client A'),
     Client(name='Client B'),
     Client(name='Client C'),
     Client(name='Client D'),
     Client(name='Client E')])
 
-session.commit()
+db.session.commit()
 
-session.add_all([
+db.session.add_all([
     Product_Area(name='Policies'),
     Product_Area(name='Billing'),
     Product_Area(name='Claims'),
     Product_Area(name='Reports')])
 
-session.commit()
+db.session.commit()
 
-session.add(Request(
+db.session.add(Request(
     title="Test request",
     description="Just populate the request table with one query to ease development",
     target_date = datetime.datetime.now(),
     priority=1,
     client_id=1,
     product_area_id=1))
-session.commit()
+db.session.commit()
